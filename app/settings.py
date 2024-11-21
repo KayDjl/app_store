57c87424603e3520cbf303b24226b75807562414
Sergey <sergkadet@yandex.ru>
2024-11-21 12:59:19 +0300

    Register simple tag for categories

--- app/settings.py
+++ app/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+from django.conf.global_settings import MEDIA_ROOT, MEDIA_URL
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -135,6 +137,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = (
     BASE_DIR / 'static',
     )
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
 
 INTERNAL_IPS = [
     # ...
