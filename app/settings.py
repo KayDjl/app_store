6ced7f50e2701c3ac02f238b86f503d096fabfc3
Sergey <sergkadet@yandex.ru>
2024-11-22 15:19:48 +0300

    PostgreSQL data base connected & full text search

--- app/settings.py
+++ app/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
 
 #Сторонние библиотеки 
     "debug_toolbar",
@@ -92,8 +93,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'home',
+        'USER': 'home',
+        'PASSWORD': 'serg2404za',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
