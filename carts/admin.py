518a6a1e32ca92b5d03904033249e1e41263daa7
Sergey <sergkadet@yandex.ru>
2024-12-01 15:47:21 +0300

    Admin panel setup

--- carts/admin.py
+++ carts/admin.py
@@ -2,4 +2,26 @@ from django.contrib import admin
 
 from carts.models import Cart
 
-admin.site.register(Cart)
+#admin.site.register(Cart)
+class CartTabAdmin(admin.TabularInline):
+    model = Cart
+    fields = "product", "quantity", "created_timesamp"
+    search_fields = "product", "quantity", "created_timesamp"
+    readonly_fields = ("created_timesamp",)
+    extra = 1
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['user_display', 'product_display', 'quantity', 'created_timesamp']
+    list_filter = ['created_timesamp', 'user', 'product__name']
+
+    def user_display(self, obj):
+        if obj.user:
+            return str(obj.user)
+        return "Анонимный пользователь"
+
+    def product_display(self, obj):
+        return str(obj.product.name)
+
+
--- goods/admin.py
+++ goods/admin.py
@@ -9,7 +9,21 @@ from goods.models import Categories, Products
 @admin.register(Categories)
 class CategoriesAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
+    list_display = ['name',]
 
 @admin.register(Products)
 class ProductsAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
+    list_display = ['name', 'quantity', 'price', 'discount']
+    list_editable = ['quantity', 'price', 'discount',]
+    search_fields = ['name', 'descriptions']
+    list_filter = ['discount', 'quantity', 'category']
+    fields = [
+        "name",
+        "category", 
+        "slug",
+        "descriptions",
+        "image",
+        ("price", "discount"),
+        "quantity",
+    ]
